841e190c0a4f45ecb6b04cb6c878ff5168b51f60
Aaryan Rathod <aryanrathod761437@gmail.com>
2026-05-26 18:29:38 +0530

    LRU Cache files created

---
+++ engine/LRUCache.cpp
@@ -0,0 +1,34 @@
+#include "LRUCache.h"
+
+LRUCache::LRUCache(int cap) {
+    capacity = cap;
+}
+
+vector<pair<double, int>> LRUCache::get(string query) {
+    // If query not in cache, return empty vector
+    if(cache_map.find(query) == cache_map.end()) {
+        return {};
+    }
+    // Splice instantly moves the node ot the front of the doubly linked list
+    lru_list.splice(lru_list.begin(), lru_list, cache_map[query].second); 
+    
+    return cache_map[query].first;
+}
+
+void LRUCache::put(string query, vector<pair<double, int>> results) {
+    // If the query already exists, update its results and move it to front
+    if(cache_map.find(query) != cache_map.end()) {
+        cache_map[query].first = results;
+        lru_list.splice(lru_list.begin(), lru_list, cache_map[query].second);
+        return;
+    }
+    // If the cache is full, pop the oldest query from the back of the list and erase from the map
+    if(lru_list.size() == capacity) {
+        string last_key = lru_list.back();
+        lru_list.pop_back();
+        cache_map.erase(last_key);
+    }
+    // Insert the new query at the front of the list and map it to its results and its new list iterator
+    lru_list.push_front(query);
+    cache_map[query] = {results, lru_list.begin()};
+}
---
+++ engine/LRUCache.h
@@ -0,0 +1,20 @@
+#pragma once
+#include<iostream>
+#include<list>
+#include<unordered_map>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+class LRUCache{
+    private:
+        int capacity;
+        list<string> lru_list;
+        unordered_map<string, pair<vector<pair<double, int>>, list<string>::iterator>> cache_map;
+    
+    public:
+        LRUCache(int cap);
+        vector<pair<double, int>> get(string query);
+        void put(string query, vector<pair<double, int>> results);
+};
